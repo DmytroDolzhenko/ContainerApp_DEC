f4104dadd72a8a9decd3bf8db5b46f1bb68f25d7
DmytroDolzhenko <dolzenkodima14@gmail.com>
2026-01-29 14:42:55 +0200

    Domain_init

---
+++ ContainerApp/src/Domain/Container/Container.cs
@@ -0,0 +1,23 @@
+﻿using Domain.ContainerType;
+using Domain.Product;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Container
+{
+    public class Container
+    {
+        public ContainerId Id { get; }
+        public ContainerTypeId TypeId { get; }
+        public ProductId? ProductId { get; set; }
+        public string Name { get; set; }
+        public string UniqCode { get; }
+        public double Capacity { get; }
+        public string Description { get; set; }
+        public bool Status { get; set; }
+    }
+    
+}
---
+++ ContainerApp/src/Domain/Container/ContainerId.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Container
+{
+    public record ContainerId(Guid value)
+    {
+        public static ContainerId New() => new(Guid.NewGuid());
+        public static ContainerId Empty() => new(Guid.Empty);
+        public override string ToString() => value.ToString();
+    }
+}
