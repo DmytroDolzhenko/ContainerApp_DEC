4d1c637bd8a560c3fe2fe172b2ccfef03405672a
DmytroDolzhenko <dolzenkodima14@gmail.com>
2026-02-02 14:05:11 +0200

    upd

--- ContainerApp/src/Infrastructure/Persistence/ApplicationDbContext.cs
+++ ContainerApp/src/Infrastructure/Persistence/ApplicationDbContext.cs
@@ -1,12 +1,34 @@
-﻿using System;
+﻿using Domain.ContainerHistories;
+using Domain.ContainerTypes;
+using Domain.Products;
+using Domain.ProductTypes;
+using Domain.Users;
+using LanguageExt.Pipes;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Infrastructure.Persistence
 {
-    internal class ApplicationDbContext
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : DbContext(options)
     {
+        public DbSet<Container> Containers { get; init; }
+        public DbSet<ContainerType> ContainerTypes { get; init; }
+        public DbSet<ContainerHistory> ContainerHistory { get; init; }
+        public DbSet<User> Users { get; init; }
+        public DbSet<Product> Products { get; init; }
+        public DbSet<ProductType> ProductTypes { get; init; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
--- ContainerApp/src/Infrastructure/Persistence/Queries/ContainerQueries.cs
+++ ContainerApp/src/Infrastructure/Persistence/Queries/ContainerQueries.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿using Application.Common.Interfaces.Queries;
+using Domain.Containers;
+using Domain.ContainerTypes;
+using Domain.Products;
+using Domain.ProductTypes;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +12,37 @@ using System.Threading.Tasks;
 
 namespace Infrastructure.Persistence.Queries
 {
-    internal class ContainerQueries
+    public class ContainerQueries : IContainerQueries
     {
+        private readonly ApplicationDbContext _context;
+        public ContainerQueries(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IReadOnlyList<Container>> GetAllAsync(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+
+        }
+
+        public Task<Container> GetByContainerType(ContainerTypeId containerTypeId, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Container> GetByIdAsync(ContainerId id, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IReadOnlyList<Container>> GetByProductAsync(ProductId productId, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IReadOnlyList<Container>> GetByProductTypeAsync(ProductTypeId productTypeId, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
